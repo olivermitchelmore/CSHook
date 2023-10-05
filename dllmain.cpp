06863bc0b1b1b0969f55dea5f5fdd0d0c0c56034
olivermitchelmore <83174660+olivermitchelmore@users.noreply.github.com>
2023-10-05 19:42:33 +0000

    update

--- dllmain.cpp
+++ dllmain.cpp
@@ -1,8 +1,12 @@
 #include <Windows.h>
 
+void initialize() {
+
+}
+
 BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReserved) {
     if (ul_reason_for_call == DLL_PROCESS_ATTACH) {
-
+        CreateThread(0, 0, reinterpret_cast<LPTHREAD_START_ROUTINE>(initialize), nullptr, 0, nullptr);
     }
 
     return TRUE;
